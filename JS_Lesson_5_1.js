739e30891eefde0b0d9d335bccf40d3948df1e36
Artem Stadnyk <qszacv@gmail.com>
2021-11-11 01:38:14 +0200

    correction

--- JS_Lesson_5_1.js
+++ JS_Lesson_5_1.js
@@ -1,22 +1,41 @@
 function makeBuffer() {
-   let text = '';
+	let bufferValue = "";
 
-    return function (add_text) {
-        if (add_text === undefined){
-            return text
-        } 
-        text += add_text;
-    }
+	function stringBuffer() {
+		return bufferValue;
+	}
+
+	stringBuffer.add = function(value) {
+		if (isNaN(value))
+			bufferValue += value + " ";
+		else
+			bufferValue += value;
+	}
+
+	stringBuffer.print = stringBuffer; 
+
+	stringBuffer.clear = function() {
+		bufferValue = "";
+	}
+
+	return stringBuffer;
 }
+
 let buffer = makeBuffer();
-buffer('JavaScrip');
-buffer('Вчити');
-buffer('Потрібно!');
-alert (buffer());
+buffer.add("JavaScript");
+buffer.add("Вчити");
+buffer.add("Потрібно!");
+console.log(buffer.print());
+
+let buffer1 = makeBuffer();
+buffer1.add(0);
+buffer1.add(1);
+buffer1.add(0);
+console.log(buffer1.print());
 
 let buffer2 = makeBuffer();
-buffer2(0);
-buffer2(1);
-buffer2(0);
-
-alert(buffer2());
+buffer2.add("Тест");
+buffer2.add("тебе не з'їсть!");
+console.log(buffer2.print());
+buffer2.clear();
+console.log(buffer2.print());
